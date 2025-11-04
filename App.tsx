0f1876c79e4d413c8ba2eb6f5a05ef6117ab5fab
sakatogluoguzcan <oguzcan.sakatoglu@ekodenge.com>
2025-11-04 19:10:36 +0300

    Refactor imports in App.tsx for better organization; update README.md with XCode and Git commands; add DEVELOPMENT_TEAM to project.pbxproj for iOS configuration.

--- App.tsx
+++ App.tsx
@@ -5,12 +5,13 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, View, useColorScheme } from 'react-native';
+
+import { NewAppScreen } from '@react-native/new-app-screen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
